c8145e62def9b8378a07b461f02c76face89f5fb
Marius S <106129151+msdev1650@users.noreply.github.com>
2023-09-27 00:20:28 +0200

    Update CheckPatreonTiersGUI.py
    This update enhances the UI and adds new features. Specifically:
    
    -Added "Copy Tier ID" and "Copy Name" buttons for each Patreon tier for easier data copying.
    
    -Introduced a "Paste Clipboard" button for the Creator Access Token field for quick token input.
    
    -Adjusted UI elements, including reducing the gap between buttons and adding a visually appealing background to the tier list.

--- CheckPatreonTiersGUI.py
+++ CheckPatreonTiersGUI.py
@@ -1,5 +1,5 @@
 import patreon
-from tkinter import Tk, Label, Entry, Button, StringVar, Text, Scrollbar, messagebox, Menu
+from tkinter import Tk, Label, Entry, Button, StringVar, Scrollbar, Frame, Canvas, messagebox, Menu
 import webbrowser
 
 def open_web_link(event):
@@ -7,7 +7,19 @@ def open_web_link(event):
 
 def show_context_menu(event):
     context_menu.post(event.x_root, event.y_root)
-    
+
+def copy_to_clipboard(text):
+    root.clipboard_clear()
+    root.clipboard_append(text)
+    root.update()
+
+def paste_from_clipboard():
+    try:
+        clipboard_content = root.clipboard_get()
+        token_var.set(clipboard_content)
+    except:
+        messagebox.showwarning("Clipboard Empty", "There is nothing to paste from clipboard.")
+
 def get_tiers():
     access_token = token_var.get().strip()
     if not access_token:
@@ -17,33 +29,51 @@ def get_tiers():
     try:
         api_client = patreon.API(access_token)
         response = api_client.fetch_user()
-
         response_data = getattr(response, 'json_data', None)
 
         if not response_data or 'included' not in response_data:
             messagebox.showerror("Error", "Error retrieving tiers")
             return
 
-        tier_text.delete(1.0, "end")
+        for widget in tier_frame.winfo_children():
+            widget.destroy()
+
         for resource in response_data['included']:
             if resource['type'] == 'reward':
                 tier_id = resource['id']
                 attributes = resource['attributes']
                 tier_name = attributes.get('title')
-                
-                if tier_id == '-1':
-                    tier_name = "Special Case -1 (Unknown)"
-                elif tier_id == '0':
-                    tier_name = "Non-Paying Users"
-                elif not tier_name or tier_name.isspace():
-                    tier_name = "*Unnamed*"
-                
-                tier_text.insert("end", f"ID: {tier_id}, Name: {tier_name}\n")
+
+                single_tier_frame = Frame(tier_frame, bg="#f5f5f5")
+                single_tier_frame.pack(fill="x", padx=5, pady=5)
+
+                tier_label_text = f"ID: {tier_id}"
+
+                if tier_id == '0':
+                    tier_label_text += ", *Non-Paying Users*"
+                elif tier_id == '-1':
+                    tier_label_text += ", *Special Case*"
+                elif int(tier_id) > 0:
+                    tier_label_text += f", Name: {tier_name if tier_name and not tier_name.isspace() else '*Unnamed*'}"
+
+                tier_label = Label(single_tier_frame, text=tier_label_text, anchor="w", bg="#f5f5f5")
+                tier_label.pack(side="left")
+
+                button_frame = Frame(single_tier_frame, bg="#f5f5f5")
+                button_frame.pack(side="right")
+
+                copy_id_button = Button(button_frame, text="Copy Tier ID", command=lambda id=tier_id: copy_to_clipboard(id))
+                copy_id_button.pack(side="left", padx=5)
+
+                if int(tier_id) > 0:
+                    copy_name_button = Button(button_frame, text="Copy Name", command=lambda name=tier_name: copy_to_clipboard(name))
+                    copy_name_button.pack(side="right", padx=5)
+
     except AttributeError as e:
         messagebox.showerror("Error", f"An error occurred: {e}")
 
 def create_gui():
-    global token_var, tier_text, context_menu
+    global token_var, tier_frame, context_menu, root
     root = Tk()
     root.title("Patreon Tier Checker")
 
@@ -61,14 +91,27 @@ def create_gui():
     context_menu.add_command(label="Paste", command=lambda: token_var.set(root.clipboard_get()))
     entry.bind("<Button-3>", show_context_menu)
 
-    Button(root, text="Retrieve Tiers", command=get_tiers).pack(pady=10)
+    button_frame = Frame(root)
+    button_frame.pack(pady=10)
+
+    retrieve_button = Button(button_frame, text="Retrieve Tiers", command=get_tiers)
+    retrieve_button.pack(side="left", padx=(0, 5))
+
+    paste_button = Button(button_frame, text="Paste Clipboard", command=paste_from_clipboard)
+    paste_button.pack(side="right", padx=(5, 0))
 
     scroll = Scrollbar(root)
     scroll.pack(side="right", fill="y")
 
-    tier_text = Text(root, wrap="none", yscrollcommand=scroll.set, width=60, height=10)
-    tier_text.pack(pady=5)
-    scroll.config(command=tier_text.yview)
+    canvas = Canvas(root, bg="#f5f5f5")
+    canvas.pack(side="left", fill="both", expand=True)
+
+    tier_frame = Frame(canvas, bg="#f5f5f5")
+    canvas.create_window((0, 0), window=tier_frame, anchor="nw")
+
+    tier_frame.bind("<Configure>", lambda event: canvas.configure(scrollregion=canvas.bbox("all")))
+    scroll.config(command=canvas.yview)
+    canvas.config(yscrollcommand=scroll.set)
 
     root.mainloop()
 
